bf4a734ef5034ae89af911911edb95fb59508750
lazaaro01 <lazaro13.vascon@gmail.com>
2026-01-19 13:15:14 -0300

    improvement: deixando mais limpo o model do appointment.go

--- internal/models/appointment.go
+++ internal/models/appointment.go
@@ -8,10 +8,16 @@ import (
 
 type Appointment struct {
 	gorm.Model
-	UserID    uint      `json:"user_id"`
-	User      User      `json:"user" gorm:"foreignKey:UserID"`
-	ServiceID uint      `json:"service_id"`
-	Service   Service   `json:"service" gorm:"foreignKey:ServiceID"`
+	ID        uint           `json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+
+	UserID    uint    `json:"user_id"`
+	User      User    `json:"user" gorm:"foreignKey:UserID"`
+	ServiceID uint    `json:"service_id"`
+	Service   Service `json:"service" gorm:"foreignKey:ServiceID"`
+
 	StartTime time.Time `json:"start_time"`
 	EndTime   time.Time `json:"end_time"`
 	Status    string    `json:"status"`
